49637b5c2aa06a8ed733cd0313541728e161af19
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2023-07-15 18:18:04 -0400

    Add wezterm conf

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+return {
+  audible_bel = "Disabled"
+  color_scheme = "Ayu Mirage"
+  font = wezterm.font_with_fallback {
+    {
+      family = "Cascadia Code PL",
+      weight = "Bold",
+      harfbuzz_features = { "calt=1", "liga=1", "ss01=1", "ss02=1", "ss19=1" },
+    },
+  },
+  font_size = 14.0,
+  hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    {
+      key = "r",
+      mods = "CTRL",
+      action = wezterm.action.ReloadConfiguration,
+    }
+  }
+}
