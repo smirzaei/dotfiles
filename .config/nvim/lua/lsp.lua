89c064064777a64147690ae8441990efd9002b53
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2023-07-18 08:22:39 -0400

    Change lsp hover key

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -30,8 +30,8 @@ lsp.on_attach(function (client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("i", "<C-h>", function () vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<C-h>", function () vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("i", "<C-h>", function () vim.lsp.buf.hover() end, opts)
 end)
 
 lsp.setup()
