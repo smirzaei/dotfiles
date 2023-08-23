f822e8069115fb21f5898df4421dbba971c8190d
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2023-08-22 22:11:11 -0400

    Create a simple command to format then write

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,3 +23,12 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Custom Commands
+function format_then_write(opts)
+  vim.cmd.Format()
+  vim.cmd.write()
+end
+
+vim.api.nvim_create_user_command('FW', format_then_write, {})
+
+
