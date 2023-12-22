50093537aa16be765af2f4d895aa1156e8b35ee8
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2023-12-21 22:20:05 -0500

    feat(wezterm): update style

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,20 @@
 local wezterm = require 'wezterm'
 
 return {
+  default_cursor_style = "SteadyBar",
   audible_bell = "Disabled",
-  color_scheme = "nordic",
+  -- color_scheme = "nordic",
   -- color_scheme = "Everforest Dark (Gogh)",
   -- color_scheme = "GitHub Dark",
+  color_scheme = "Gruvbox Material (Gogh)",
+  -- color_scheme = "AyuDark (Gogh)",
   font = wezterm.font_with_fallback {
     {
-      family = "Cascadia Code PL",
+      -- family = "Cascadia Code PL",
+      family = "CaskaydiaCove Nerd Font",
       weight = "Bold",
-      harfbuzz_features = { "calt=1", "liga=1", "ss01=1", "ss02=1", "ss19=1" },
+      harfbuzz_features = { "calt=1", "liga=1", "ss02=1", "ss19=1" },
+      -- harfbuzz_features = { "calt=1", "liga=1", "ss01=1", "ss02=1", "ss19=1" },
     },
   },
   font_size = 14.0,
