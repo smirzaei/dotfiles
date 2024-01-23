21926351e0536c6aaaafb3f98c9ec629752dcb14
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-22 19:42:33 -0500

    style(nvim): set indentation to 4 spaces

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -33,7 +33,6 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 
 -- Shamelessly taken from: https://github.com/LunarVim/Neovim-from-scratch/blob/01-options/lua/user/options.lua
-
 local options = {
   backup = false,                          -- creates a backup file
   swapfile = false,                        -- creates a swapfile
@@ -50,7 +49,7 @@ local options = {
   mouse = 'a',                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
   showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
+  showtabline = 4,                         -- always show tabs
   smartcase = true,                        -- smart case
   smartindent = true,                      -- make indenting smarter again
   splitbelow = true,                       -- force all horizontal splits to go below current window
@@ -60,8 +59,8 @@ local options = {
   updatetime = 150,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+  tabstop = 4,                             -- insert 2 spaces for a tab
   softtabstop = 2,
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
