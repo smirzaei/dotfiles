70026b9473e345853445beadd113e5f87f89eb34
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-22 19:40:32 -0500

    style(nvim): show warn, err, etc. icons in gutter

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -94,6 +94,13 @@ local config_lsp = function()
 
 
 end
+
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 return {
   {
     'neovim/nvim-lspconfig',
