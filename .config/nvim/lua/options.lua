20cbede0bec5cf8b37731f5f6562ccc74a8dadd1
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-27 14:51:47 -0500

    refactor(nvim): overhaul options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,87 +4,46 @@ vim.api.nvim_create_autocmd('BufEnter', {
     vim.opt_local.formatoptions:remove({ 'r', 'o' })
 
     -- Disable mouse mode
-    vim.o.mouse = ''
+    vim.opt.mouse = ''
   end
 })
 
--- Make line numbers default
-vim.wo.number = true
 
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- Shamelessly taken from: https://github.com/LunarVim/Neovim-from-scratch/blob/01-options/lua/user/options.lua
-local options = {
-  backup = false,                          -- creates a backup file
-  swapfile = false,                        -- creates a swapfile
-  undodir = os.getenv('HOME') .. '/.vim/undodir',
-  undofile = true,                         -- enable persistent undo
-  clipboard = 'unnamedplus',               -- allows neovim to access the system clipboard
-  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
-  completeopt = { 'menuone', 'noselect' }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = 'utf-8',                  -- the encoding written to a file
-  hlsearch = false,                        -- highlight all matches on previous search pattern
-  incsearch = true,
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = 'a',                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 4,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  updatetime = 150,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  softtabstop = 2,
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  nu = true,
-
-  signcolumn = 'yes',                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
-  scrolloff = 15,                           -- minimal number of screen lines to keep above and below the cursor
-  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
-  whichwrap = 'bs<>[]hl',                  -- which 'horizontal' keys are allowed to travel to prev/next line
-  colorcolumn = '80',
-}
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
-
--- vim.opt.shortmess = 'ilmnrx'                        -- flags to shorten vim messages, see :help 'shortmess'
-vim.opt.shortmess:append 'c'                           -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append '-'                           -- hyphenated words recognized by searches
-vim.opt.formatoptions:remove({ 'c', 'r', 'o' })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
-vim.opt.runtimepath:remove('/usr/share/vim/vimfiles')  -- separate vim plugins from neovim in case vim still in use
-
-
+vim.opt.completeopt = "menu,menuone,preview,noselect"
+vim.opt.confirm = true -- confirm before exit
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.formatoptions = "tcqnljp" -- text format and line breaks. see :h formatoptions
+vim.opt.grepprg = "rg --vimgrep" -- use ripgrep
+vim.opt.ignorecase = true
+vim.opt.inccommand = "nosplit"
+vim.opt.laststatus = 3  -- show a global status line
+vim.list = false -- Don't show hidden characters
+vim.opt.mouse = "" -- disable mouse
+vim.opt.number = true -- Show line numbers
+vim.opt.pumblend = 0 -- popup transparency 0-100
+vim.opt.pumheight = 12 -- limit popup height
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 15 -- how many lines to keep above and below the cursor
+vim.opt.shiftround = true -- round indent to multiple of shiftwidth
+vim.opt.shiftwidth = 4 -- number of spaces for indent
+vim.opt.shortmess:append({x = true})
+vim.opt.showmode = false -- we use a status line
+vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true -- don't ignore case with capitals
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.spelllang = "en_us"
+vim.opt.splitbelow = true -- put new splits below the current
+vim.opt.splitright = true -- put new splits to the right of the current
+vim.opt.tabstop = 4 -- number of spaces for each tab
+vim.opt.termguicolors = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.virtualedit = "block"
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wrap = false -- disable line wrap
+-- vim.opt.smoothscroll = true -- available in neovim 0.10
+vim.opt.colorcolumn = "80,100"
