54c67d3912147206b28e4e3cda5d03cff830891d
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-27 15:03:01 -0500

    chore(nvim): remove duplicate diagnostics bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,12 +7,6 @@ require('lazy').setup({
   { import = 'plugins' },
 })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
 -- Custom Commands
 function format_then_write(opts)
   vim.cmd.Format()
