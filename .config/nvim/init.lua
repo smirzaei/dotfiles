8cf3010e06971d49fb3cef03a8dfe403e177ee1b
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-27 14:51:07 -0500

    refactor(nvim): move auto commands in its own file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,10 @@
 require('options')
 require('keybindings')
+require('autocmds')
 require('init_lazy')
 
 require('lazy').setup({
   { import = 'plugins' },
-})
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
 })
 
 -- Diagnostic keymaps
