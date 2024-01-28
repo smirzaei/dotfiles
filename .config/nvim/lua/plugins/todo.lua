0665a4ae5a9cebd8f884dcf9faadc86f8a4cd257
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-27 20:25:00 -0500

    style(nvim): update todo color

--- .config/nvim/lua/plugins/todo.lua
+++ .config/nvim/lua/plugins/todo.lua
@@ -1,5 +1,14 @@
 return {
-  'folke/todo-comments.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {}
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        colors = {
+            error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+            warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+            info = { "DiagnosticInfo", "#6CB6EB" },
+            hint = { "DiagnosticHint", "#10B981" },
+            default = { "Identifier", "#7C3AED" },
+            test = { "Identifier", "#FF00FF" },
+        },
+    },
 }
