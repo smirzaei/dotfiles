e03d36506880cb054472625f380b0438e1b3fe1a
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-02-12 17:42:18 -0500

    style(nvim): show file path in status line

--- .config/nvim/lua/plugins/line.lua
+++ .config/nvim/lua/plugins/line.lua
@@ -33,7 +33,9 @@ local lualine_cfg = {
   sections = {
     lualine_a = {icon, 'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {'filename', path = 1}
+    },
     lualine_x = {'encoding', file_format, 'filetype' },
     lualine_y = {'progress'},
     lualine_z = {'location'}
