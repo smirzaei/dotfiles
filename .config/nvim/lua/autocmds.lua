e99e44b3fac7a15b6754840cb29e4a1a59087fb6
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-01-14 20:04:56 -0500

    chore(nvim): remove show/hide line numbers in terminal

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -19,27 +19,6 @@ vim.api.nvim_create_autocmd("VimResized", {
 	end,
 })
 
--- Hide line numbers in terminal
-local term_group = vim.api.nvim_create_augroup("Terminal", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-	group = term_group,
-	callback = function()
-		vim.opt_local.number = false
-		vim.opt_local.relativenumber = false
-	end,
-})
-
--- Show line numbers when not in terminal
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = term_group,
-	callback = function()
-		if vim.bo.buftype ~= "terminal" then
-			vim.opt_local.number = true
-			vim.opt_local.relativenumber = true
-		end
-	end,
-})
-
 -- show a column at 80 characters as a guide for long lines
 --- except in Rust where the rule is 100 characters
 vim.api.nvim_create_autocmd("Filetype", { pattern = "rust", command = "set colorcolumn=100" })
