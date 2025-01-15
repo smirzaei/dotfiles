170df16027698a294ff89379e1d4bc12a6d1fb32
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-01-15 09:06:46 -0500

    feat(fish): add stuff to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,28 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # PATH Set up
+
+    set PATH $PATH $HOME/.local/bin/
+
+    # Go is installed or not
+    if type -q go
+        # Is $GOPATH set
+        if set gopath (go env GOPATH)
+            if test -n "$gopath"
+                set PATH $PATH $gopath/bin
+            end
+        end
+    end
+
+    if test -d $HOME/.cargo/bin
+        set PATH $PATH $HOME/.cargo/bin/
+    end
+
+    if test -d $HOME/.krew
+        set KREW_ROOT $HOME/.krew/
+        set PATH $PATH $HOME/.krew/bin/
+    end
 end
 
 starship init fish | source
