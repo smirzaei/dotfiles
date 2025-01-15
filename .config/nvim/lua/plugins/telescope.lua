ac034b1321d584650468a6924167649e815ecfc3
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-01-14 19:00:05 -0500

    feat(nvim): theme switcher keybinding

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -22,6 +22,7 @@ return {
       vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>ts', require('telescope.builtin').colorscheme, { desc = '[T]heme [S]witch' })
     end
   },
   {
