90f90e9800b10d6b9920c0cfa910018ad79147e0
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-01-17 17:33:43 -0500

    feat(nvim): change mode display in status line

--- .config/nvim/lua/plugins/line.lua
+++ .config/nvim/lua/plugins/line.lua
@@ -1,59 +1,100 @@
 local file_format = {
-  "fileformat",
-  symbols = {
-    unix = "unix",
-    dos = "dos",
-    mac = "mac",
-  },
+	"fileformat",
+	symbols = {
+		unix = "unix",
+		dos = "dos",
+		mac = "mac",
+	},
 }
 
+local mode_map = {
+	["n"] = "N",
+	["no"] = "O·P",
+	["nov"] = "O·P",
+	["noV"] = "O·P",
+	["no\22"] = "O·P",
+	["niI"] = "N·I",
+	["niR"] = "N·R",
+	["niV"] = "N",
+	["nt"] = "N·T",
+	["v"] = "V",
+	["vs"] = "V",
+	["V"] = "V·L",
+	["Vs"] = "V·L",
+	["\22"] = "V·B",
+	["\22s"] = "V·B",
+	["s"] = "S",
+	["S"] = "S·L",
+	["\19"] = "S·B",
+	["i"] = "I",
+	["ic"] = "I·C",
+	["ix"] = "I·X",
+	["R"] = "R",
+	["Rc"] = "R·C",
+	["Rx"] = "R·X",
+	["Rv"] = "V·R",
+	["Rvc"] = "RVC",
+	["Rvx"] = "RVX",
+	["c"] = "C",
+	["cv"] = "EX",
+	["ce"] = "EX",
+	["r"] = "R",
+	["rm"] = "M",
+	["r?"] = "C",
+	["!"] = "SH",
+	["t"] = "T",
+}
+local function short_mode()
+	return mode_map[vim.api.nvim_get_mode().mode] or "?"
+end
+
 local lualine_cfg = {
-  options = {
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = "|",
-    section_separators = "",
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    },
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = {
-      { "filename", path = 1 },
-    },
-    lualine_x = { "encoding", file_format, "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = "|",
+		section_separators = "",
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = { short_mode },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			{ "filename", path = 1 },
+		},
+		lualine_x = { "encoding", file_format, "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
 }
 
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = lualine_cfg,
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = lualine_cfg,
 }
