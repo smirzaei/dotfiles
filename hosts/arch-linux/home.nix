9afe8ddae1ec1868caf5b08645407972fdc0b46b
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-10-05 13:14:33 -0400

    feat: update fzf config

--- hosts/arch-linux/home.nix
+++ hosts/arch-linux/home.nix
@@ -97,6 +97,21 @@
     programs.fzf = {
         enable = true;
         enableZshIntegration = true;
+        defaultCommand = "fd --type f --hidden --follow --exclude .git";
+        defaultOptions = [
+            "--height 40%"
+            "--layout=reverse"
+            "--border"
+            "--inline-info"
+        ];
+        changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git"; # ALT-C
+        changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
+        fileWidgetCommand = "fd --type f --hidden --follow --exclude .git"; # ALT-T
+        fileWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
+        tmux = {
+            enableShellIntegration = true;
+            shellIntegrationOptions = [ "--height 40%" "--layout=reverse" "--border" "--inline-info" ];
+        };
     };
 
     # Github CLI
