992481fa201fa3caa4291dc6eda8cba758ab9aa6
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-10-05 13:06:59 -0400

    feat: migrate `.zshrc` to nix

--- hosts/arch-linux/home.nix
+++ hosts/arch-linux/home.nix
@@ -23,6 +23,45 @@
         wabt        # WebAssembly tools
     ];
 
+    programs.zsh = {
+        enable = true;
+        # Don’t forget to add `environment.pathsToLink = [ "/share/zsh" ];`
+        # to your system configuration to get completion for system packages (e.g. systemd).
+        enableCompletion = true;
+        autosuggestion = {
+            enable = true;
+        };
+        syntaxHighlighting = {
+            enable = true;
+        };
+        history = {
+            append = true;
+            expireDuplicatesFirst = true;
+            ignoreDups = true; # Do not enter command lines into the history list if they are duplicates of the previous event.
+            save = 100000; # Number of history lines to save.
+            size = 100000; # Number of history lines to keep.
+            saveNoDups = true; # Do not write duplicate entries into the history file.
+            share = true; # Share command history between zsh sessions.
+        };
+        shellAliases = {
+            ll = "exa -lah --color=always --group-directories-first";
+            l = "exa -lah --color=always --group-directories-first";
+            c = "clear";
+            g = "git";
+            ga = "git add";
+            gst = "git status";
+            gd = "git diff";
+            gc = "git commit";
+            gp = "git push";
+            gl = "git pull";
+            k = "kubectl";
+            kns = "kubens";
+            kctx = "kubectx";
+            fk = ''kill -9 $(ps aux | fzf | awk "{print \$2}")'';
+            fp = "ps aux | fzf";
+        };
+    };
+
     programs.git = {
         enable = true;
         userEmail = private.personalEmail;
