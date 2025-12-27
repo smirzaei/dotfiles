0808320d5bac836b2eeddbc8b9230b3dbf54985c
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-12-27 15:37:18 -0500

    nvim: enable rust-analyzer for all platforms

--- home/.config/nvim/lua/plugins/rust.lua
+++ home/.config/nvim/lua/plugins/rust.lua
@@ -1,4 +1,25 @@
 return {
 	"mrcjkb/rustaceanvim",
 	lazy = false, -- this plugin is already lazy
+	config = function()
+		vim.g.rustaceanvim = {
+			server = {
+				default_settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+						check = {
+							targets = {
+								"x86_64-unknown-linux-gnu",
+								"aarch64-apple-darwin",
+								"x86_64-pc-windows-msvc",
+							},
+							command = "clippy",
+						},
+					},
+				},
+			},
+		}
+	end,
 }
