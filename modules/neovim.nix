55eeca7f9843547d360ceb1e48fda4ecd1134e81
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2026-06-21 09:29:29 -0400

    nix: keep neovim's vim.pack lockfile writable

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -29,8 +29,23 @@ in
 
     config = lib.mkIf cfg.enable {
         home.packages = lib.optional (cfg.package != null) cfg.package;
-        xdg.configFile."nvim".source = ../home/.config/nvim;
-        xdg.configFile."nvim".recursive = true;
+
+        # Neovim vim.pack currently writes nvim-pack-lock.json under stdpath("config")
+        # with no configurable lockfile path. Home Manager's recursive source would
+        # put that file in the read-only Nix store, so keep only the lockfile writable
+        # until https://github.com/neovim/neovim/issues/36078 is resolved.
+        xdg.configFile."nvim" = {
+            source = builtins.path {
+                name = "hm_nvim";
+                path = ../home/.config/nvim;
+                filter = path: type: type != "regular" || builtins.baseNameOf path != "nvim-pack-lock.json";
+            };
+            recursive = true;
+        };
+
+        xdg.configFile."nvim/nvim-pack-lock.json".source =
+            config.lib.file.mkOutOfStoreSymlink
+                "${config.home.homeDirectory}/projects/smirzaei/dotfiles/home/.config/nvim/nvim-pack-lock.json";
 
         home.activation.checkNeovimBinary = lib.hm.dag.entryAfter [ "installPackages" ] ''
             PATH="${config.home.profileDirectory}/bin:${"$"}PATH"
